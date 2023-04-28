ba6736d8279aa791f6d3735ccb3e75f991679774
TongFrank <2741187147@qq.com>
2023-04-28 16:47:47 +0800

    echo "# WebLearning" >> README.md git init git add README.md git commit -m "first commit" git branch -M main git remote add origin https://github.com/TongFrank/WebLearning.git git push -u origin main

---
+++ main.js
@@ -0,0 +1,55 @@
+//封面显示日期和时间
+function getDate() {
+    let date = new Date();
+    let year = date.getFullYear(); //获取年份
+    let month = date.getMonth() + 1; //获取月份
+    let day = date.getDate(); //获取日期
+    return year + ' 年 ' + month + ' 月 ' + day + ' 日 '
+}
+function getTime() {
+    let date = new Date();
+    let hour = date.getHours(); //获取小时
+    hour = hour < 10 ? '0' + hour : hour;
+    let minute = date.getMinutes(); // 获取分
+    minute = minute < 10 ? '0' + minute : minute;
+    let seconds = date.getSeconds(); //获取秒
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+    return hour + ' : ' + minute + ' : ' + seconds;
+}
+setInterval(function() {
+    document.querySelector('#date').innerHTML = getDate();
+    document.querySelector('#time').innerHTML = getTime();
+}, 1000)
+
+//滑动条在顶部时，导航栏透明
+window.addEventListener('scroll', function(){
+    let homeHead = window.document.querySelector(".home-head")
+    let prehead = this.scrollY
+    if (window.scrollY !== 0){
+       homeHead.classList.add("navTran") 
+    }else{
+       homeHead.classList.remove("navTran")
+    }
+})
+
+
+//监控选择器infor以控制导航栏隐藏出现行为
+function hiddenNav(entries) {
+    // 拿到 IntersectionObserverEntry 对象
+    const informEntry = entries[0];
+
+    if (informEntry.intersectionRatio == 0) {
+        navg.classList.add('hidden');
+    } else {
+        navg.classList.remove('hidden');
+    }
+}
+
+const inform = document.querySelector(".info");
+const navg = document.querySelector("nav");
+// 使用[交叉观察器]
+const io = new IntersectionObserver(hiddenNav);
+// 监控选择器infor
+io.observe(inform);
+
+
